4845891a2f967e42c831b22b8c763286b303205a
Danilo <daniloczar@hotmail.com>
2024-05-29 21:43:14 +0100

    add comments model and controller

---
+++ controllers/comments.controller.js
@@ -0,0 +1,33 @@
+const {
+  selectArticleCommentsByArticleId,
+  checkArticleExists, 
+  insertCommentByArticleId,
+} = require("../models/comments.model");
+
+
+
+  exports.getArticleCommentsByArticleId = (req, res, next) => {
+    const { article_id } = req.params;
+    checkArticleExists(article_id)
+      .then(() => {
+        return selectArticleCommentsByArticleId(article_id);
+      })
+      .then((comments) => {
+        res.status(200).send({ comments });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  };
+exports.postCommentByArticleId = (req, res, next) => {
+    const {article_id} = req.params
+    const {username, body} = req.body
+
+    insertCommentByArticleId(article_id, username, body)
+    .then((postComment) => {
+        res.status(201).send({ postComment });
+    })
+    .catch((error) => {
+        next(error)
+    })
+}
---
+++ models/comments.model.js
@@ -0,0 +1,36 @@
+const db = require("../db/connection.js");
+exports.selectArticleCommentsByArticleId = (article_id) => {
+  let queryString = `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`;
+
+  return db.query(queryString, [article_id]).then(({ rows }) => {
+    return rows;
+  });
+};
+exports.checkArticleExists = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      }
+    });
+};
+
+exports.insertCommentByArticleId = (article_id, username, body) => {
+     if (typeof body !== "string" || typeof username !== "string") {
+       return Promise.reject({ status: 400, msg: "bad request" });
+     } 
+  return db
+    .query(
+      `
+    INSERT INTO comments
+    (body, author, article_id)
+    VALUES
+    ($1, $2, $3)
+    RETURNING *`, [body, username, article_id]
+    )
+    .then(({ rows }) => {
+       console.log('----->',rows)
+      return rows[0];
+    });
+};
