21fde90c7b9d855b8a69212a293bc1d4fb40c630
Danilo <daniloczar@hotmail.com>
2024-05-29 19:44:06 +0100

    add task 6, refactored error handlers, add error to tdd

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const request = require("supertest");
 const seed = require("../db/seeds/seed");
 const db = require("../db/connection");
 const data = require("../db/data/test-data");
-const endpoints = require('../endpoints.json')
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => {
   return seed(data);
@@ -20,15 +20,9 @@ describe("GET/api/topics", () => {
       .then(({ body }) => {
         expect(body.ArrTopics.length).toBe(3);
         expect(body.ArrTopics).toMatchObject([
-          { slug: "mitch", 
-            description: "The man, the Mitch, the legend" 
-          },
-          { slug: "cats",
-            description: "Not dogs" 
-          },
-          { slug: "paper", 
-            description: "what books are made of" 
-          },
+          { slug: "mitch", description: "The man, the Mitch, the legend" },
+          { slug: "cats", description: "Not dogs" },
+          { slug: "paper", description: "what books are made of" },
         ]);
       });
   });
@@ -36,7 +30,7 @@ describe("GET/api/topics", () => {
     return request(app)
       .get("/api/topic")
       .expect(404)
-      .then(({body}) => {
+      .then(({ body }) => {
         expect(body.msg).toBe("Not found");
       });
   });
@@ -48,27 +42,27 @@ describe("GET/api", () => {
       .get("/api")
       .expect(200)
       .then(({ body }) => {
-        expect(body.endpoints).toEqual(endpoints)
-        });
-    });
-    test(`Status: 200, checks if the endpoints is returning a Object`, () => {
-      return request(app)
-        .get(`/api`)
-        .expect(200)
-        .then(({ body }) => {
-          const { endpoints } = body;
-          expect(endpoints).toBeInstanceOf(Object);
-        });
-    });
+        expect(body.endpoints).toEqual(endpoints);
+      });
+  });
+  test(`Status: 200, checks if the endpoints is returning a Object`, () => {
+    return request(app)
+      .get(`/api`)
+      .expect(200)
+      .then(({ body }) => {
+        const { endpoints } = body;
+        expect(endpoints).toBeInstanceOf(Object);
+      });
+  });
 });
 
-describe('GET/api/article/:articles_id', () => {
+describe("GET/api/article/:articles_id", () => {
   test("Responds with the appropriate article when ID is passed", () => {
     return request(app)
       .get("/api/articles/2")
       .expect(200)
-      .then(({body}) => {
-        const {article} = body
+      .then(({ body }) => {
+        const { article } = body;
         expect(article).toMatchObject({
           article_id: expect.any(Number),
           title: expect.any(String),
@@ -81,42 +75,116 @@ describe('GET/api/article/:articles_id', () => {
         });
       });
   });
+  test("returns 400 status when the article_id passed it is a different data type", () => {
+    return request(app)
+      .get("/api/articles/dog")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
 
+  test("returns 404 status when the id does not exit", () => {
+    return request(app)
+      .get("/api/articles/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
+      });
+  });
 });
 
-describe('GET/api/articles', () => {
-   test("should respond with 200 status and all articles each of which include the required properties sorted by date in descending order ", () => {
-     return request(app)
-       .get("/api/articles")
-       .expect(200)
-       .then(({ body }) => {
-         const articles = body.allArticles;
-         
-         expect(articles.length).toBe(13);
-         expect(articles).toBeInstanceOf(Array);
+describe("GET/api/articles", () => {
+  test("should respond with 200 status and all articles each of which include the required properties sorted by date in descending order ", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const articles = body.allArticles;
 
-         articles.forEach((article) => {
+        expect(articles.length).toBe(13);
+        expect(articles).toBeInstanceOf(Array);
+        
+        articles.forEach((article) => {
           expect(article).not.toHaveProperty("body");
           expect(article).toMatchObject({
-             article_id: expect.any(Number),
-             title: expect.any(String),
-             topic: expect.any(String),
-             author: expect.any(String),
-             created_at: expect.any(String),
-             votes: expect.any(Number),
-             article_img_url: expect.any(String),
-             comment_count: expect.any(String),
-           });
-         });
-       });
-   });
+            article_id: expect.any(Number),
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(String),
+          });
+          expect(typeof article.comment_count).not.toBe("number");
+        });
+      });
+  });
 
-   test("should respond with a 404 if given a not existent endpoint", () => {
-     return request(app)
-       .get("/api/articlez")
-       .expect(404)
-       .then(({ body }) => {
-         expect(body.msg).toBe("Not found");
-       });
-   });
+  test("should respond with a 404 if given a not existent ID endpoint", () => {
+    return request(app)
+      .get("/api/99999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
+      });
+  });
+
+  test("400: Bad request, INVALID id", () => {
+    return request(app)
+      .get("/api/articles/apple")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
+});
+
+describe("GET/api/articles/:article_id/comments", () => {
+  test("Responds with an array of comments", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body }) => {
+        const result = body.comments;
+        expect(Array.isArray(result)).toBe(true);
+      });
+  });
+  test("404: responds with appropriate message when given valid but non-existent id.", () => {
+    return request(app)
+      .get("/api/articles/999999/comments")
+      .expect(404)
+      .then(({ body }) => {
+        const { msg } = body;
+        expect(msg).toBe("Not Found");
+      });
+  });
+
+  test("400: Bad request, INVALID id", () => {
+    return request(app)
+      .get("/api/articles/apple/comments")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
+  test("200 and returns the comments for a determined article", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body }) => {
+        const { comments } = body;
+        comments.forEach((comment) => {
+          expect(comment).toMatchObject({
+            comment_id: expect.any(Number),
+            votes: expect.any(Number),
+            created_at: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            article_id: expect.any(Number),
+          });
+        });
+      });
+  });
 });
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,22 +1,41 @@
-const { selectArticlesId, selectAllArticles } = require('../models/articles.model')
+const {
+  selectArticlesId,
+  selectAllArticles,
+  selectArticleCommentsByArticleId,
+  checkArticleExists,
+} = require("../models/articles.model");
 
 exports.getArticlesById = (req, res, next) => {
   const { article_id } = req.params;
-    selectArticlesId(article_id)
-    .then((article)=>{
-        res.status(200).send({article})
+  selectArticlesId(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
     })
-    .catch((error)=>{
-        next(error)
-    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 exports.getAllArticles = (req, res, next) => {
-    selectAllArticles()
-    .then((allArticles)=>{
-        res.status(200).send({allArticles})
+  selectAllArticles()
+    .then((allArticles) => {
+      res.status(200).send({ allArticles });
     })
-    .catch((error)=>{
-        next(error)
-    })
-}
+    .catch((error) => {
+      next(error);
+    });
+};
+
+exports.getArticleCommentsByArticleId = (req, res, next) => {
+ const { article_id } = req.params;
+ checkArticleExists(article_id)
+   .then(() => {
+     return selectArticleCommentsByArticleId(article_id);
+   })
+   .then((comments) => {
+     res.status(200).send({ comments });
+   })
+   .catch((error) => {
+     next(error);
+   });
+};
