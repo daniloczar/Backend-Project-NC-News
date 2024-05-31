6ca48678eecc65afa2e9c02db567dfb74f3a6418
Danilo <daniloczar@hotmail.com>
2024-05-31 14:06:35 +0100

    make change package.json

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-const config = {}
-
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -16,4 +10,10 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
+const config = {}
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 module.exports = new Pool(config);
