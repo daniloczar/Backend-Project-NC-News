a26128d63faa048b7745a588dbf043d5671492fb
Danilo <daniloczar@hotmail.com>
2024-06-17 19:48:25 +0100

    cors issue

--- app.js
+++ app.js
@@ -35,6 +35,44 @@ app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
+app.options("*", (req, res) => {
+  console.log("preflight");
+  if (
+    req.headers.origin ===
+      "https://backend-project-nc-news-z6wy.onrender.com" &&
+    allowMethods.includes(req.headers["access-control-request-method"]) &&
+    allowHeaders.includes(req.headers["access-control-request-headers"])
+  ) {
+    console.log("pass");
+    return res.status(204).send();
+  } else {
+    console.log("fail");
+  }
+  })
+
+  app.get("/healthz", (req, res) => {
+    console.log("health check is processed");
+    return res.status(204).send();
+  });
+
 //customError
 app.use((error, req, res, next) => {
   if (error.status && error.msg) {
